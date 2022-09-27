96b7e65796988008015293574211b46d718f2b69
Sabrina <smg21022@gmail.com>
2022-09-26 23:39:33 -0600

    Finished questions

--- index.js
+++ index.js
@@ -30,8 +30,12 @@ console.log('example task:', processFirstItem(['foo','bar'],function(str){return
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+
+  -count is declared outside of the function in counter2 code, and inside the function in counter 1 code. 
   
   2. Which of the two uses a closure? How can you tell?
+
+
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
@@ -64,10 +68,14 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(Number){
+    for (let i = 0; i < 10 ; i++) {
+      const score = Math.floor(Math.random()* 3) + 1;
+      return score ; 
+    }
 }
 
+console.log(inning());
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
 Use the finalScore function below to do the following:
@@ -83,10 +91,20 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*Code Here*/){
-  /*Code Here*/
+function finalScore(inningCB, innings){
+ let homeScore = 0;
+ let awayScore = 0;
+ for (let i = 0; i < innings; i++){
+  homeScore = homeScore + inningCB();
+  awayScore = awayScore + inningCB ();
+ }
+ return {
+  Home: homeScore,
+  Away: awayScore
+ }
 }
 
+console.log('Task 3:', finalScore(inning, 9));
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
@@ -101,11 +119,14 @@ For example: invoking getInningScore(inning) might return this object:
   */
 
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
-
+function getInningScore(inningCB) {
+  return {
+    Home: inningCB(),
+    Away: inningCB()
+  }
 }
 
+console.log('Task 4:', getInningScore(inning));
 
 /* STRETCH: ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
 Use the scoreboard function below to do the following:
@@ -147,8 +168,8 @@ Use the scoreboard function below to do the following:
   "This game will require extra innings: Away 10 - Home 10"
 ] */
 // NOTE: There is no test associated with this code; if your output matches the given example, consider it complete!
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard() {
+
 }
 
 
